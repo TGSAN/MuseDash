83039b568ed2f6025ed936b10a8d21578d7afc1b
huangwenhui <h815599079@hotmail.com>
2016-09-23 12:56:29 +0800

    tmp

--- Assets/Scripts/Common/Util.cs
+++ Assets/Scripts/Common/Util.cs
@@ -5,10 +5,6 @@ using Object = UnityEngine.Object;
 
 public class JsonUtil
 {
-    public static Object LoadJson(string path, string key)
-    {
-        return null;
-    }
 }
 
 public class DOTweenUtil
