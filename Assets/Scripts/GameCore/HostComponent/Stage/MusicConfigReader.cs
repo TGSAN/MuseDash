7b9c4444dc4e927c245fe179c37e91641b8ffcda
huangwenhui <815599079@qq.com>
2016-10-14 14:50:16 +0800

    音乐配置完善

--- Assets/Scripts/GameCore/HostComponent/Stage/MusicConfigReader.cs
+++ Assets/Scripts/GameCore/HostComponent/Stage/MusicConfigReader.cs
@@ -1,301 +1,352 @@
-using UnityEngine;
-using System.Collections;
 using LitJson;
+using System.Collections;
+using UnityEngine;
 
-namespace GameLogic {
-	public struct NodeConfigData {
-		public static uint[] NODE_TYPE_IS_ADD_COMBO = new uint[]{
-			GameGlobal.NODE_TYPE_MONSTER,
-			GameGlobal.NODE_TYPE_HIDE,
-			GameGlobal.NODE_TYPE_PRESS,
-			GameGlobal.NODE_TYPE_BOSS,
-			GameGlobal.NODE_TYPE_AIR_BEAT
-		};
+namespace GameLogic
+{
+    public struct NodeConfigData
+    {
+        public static uint[] NODE_TYPE_IS_ADD_COMBO = new uint[]{
+            GameGlobal.NODE_TYPE_MONSTER,
+            GameGlobal.NODE_TYPE_HIDE,
+            GameGlobal.NODE_TYPE_PRESS,
+            GameGlobal.NODE_TYPE_BOSS,
+            GameGlobal.NODE_TYPE_AIR_BEAT
+        };
 
-		public static uint[] NODE_TYPE_IS_MISS_COMBO = new uint[]{
-			GameGlobal.NODE_TYPE_MONSTER,
-			GameGlobal.NODE_TYPE_PRESS,
-			GameGlobal.NODE_TYPE_BOSS,
-			GameGlobal.NODE_TYPE_AIR_BEAT
-		};
+        public static uint[] NODE_TYPE_IS_MISS_COMBO = new uint[]{
+            GameGlobal.NODE_TYPE_MONSTER,
+            GameGlobal.NODE_TYPE_PRESS,
+            GameGlobal.NODE_TYPE_BOSS,
+            GameGlobal.NODE_TYPE_AIR_BEAT
+        };
 
-		public static uint[] NODE_TYPE_IS_SHOW_PLAY_EFFECT = new uint[]{
-			GameGlobal.NODE_TYPE_MONSTER,
-			GameGlobal.NODE_TYPE_HIDE,
-			GameGlobal.NODE_TYPE_PRESS,
-			GameGlobal.NODE_TYPE_BOSS,
-			GameGlobal.NODE_TYPE_AIR_BEAT
-		};
+        public static uint[] NODE_TYPE_IS_SHOW_PLAY_EFFECT = new uint[]{
+            GameGlobal.NODE_TYPE_MONSTER,
+            GameGlobal.NODE_TYPE_HIDE,
+            GameGlobal.NODE_TYPE_PRESS,
+            GameGlobal.NODE_TYPE_BOSS,
+            GameGlobal.NODE_TYPE_AIR_BEAT
+        };
 
-		public const string JUMP_ACTION = "char_jump";
+        public const string JUMP_ACTION = "char_jump";
 
-		public string uid;
-		public string scene;
-		public string des;
-		public string prefab_path;
-		public uint hit_type;
-		public uint type;
-		public decimal life;
-		// public decimal comeoutTime;
+        public string uid;
+        public string scene;
+        public string des;
+        public string prefab_path;
+        public uint hit_type;
+        public uint type;
+        public decimal life;
+        // public decimal comeoutTime;
 
-		public string effect;
-		public string key_audio;
-		public string boss_action;
-		public string boss_hurt;
-		public string char_action;
+        public string effect;
+        public string key_audio;
+        public string boss_action;
+        public string boss_hurt;
+        public string char_action;
 
-		public decimal perfect_range;
-		public decimal great_range;
-		public decimal cool_range;
+        public decimal perfect_range;
+        public decimal great_range;
+        public decimal cool_range;
 
-		public int damage;
-		public int prop_value;
-		public float damage_growth;
+        public int damage;
+        public int prop_value;
+        public float damage_growth;
 
-		public bool enable_jump;
+        public bool enable_jump;
 
-		public bool missCombo;
-		public bool addCombo;
-		public bool jump_note;
-		public bool isShowPlayEffect;
-		public decimal[] hitRange;
-		public decimal hitRangeAll;
-	}
+        public bool missCombo;
+        public bool addCombo;
+        public bool jump_note;
+        public bool isShowPlayEffect;
+        public decimal[] hitRange;
+        public decimal hitRangeAll;
+    }
 
-	public struct MusicConfigData {
-		public int Beat;
-		public decimal Timming;
-		public bool ishide;
-		public string CubeType;
-	}
+    public struct MusicConfigData
+    {
+        public int Beat;
+        public decimal Timming;
+        public bool ishide;
+        public string CubeType;
+    }
 
-	public struct MusicData{
-		public int objId;
-		public decimal tick;
-		public MusicConfigData configData;
-		public NodeConfigData nodeData;
-		private decimal shotPause;
-		private float attackRangeRate;
-		private int size;		// size 0 is not  anything,1 is big,2 is small
-		public void SetShotPause(decimal _tick) {
-			if (_tick < 0) {
-				_tick = 0m;
-			}
+    public struct MusicData
+    {
+        public int objId;
+        public decimal tick;
+        public MusicConfigData configData;
+        public NodeConfigData nodeData;
+        private decimal shotPause;
+        private float attackRangeRate;
+        private int size;       // size 0 is not  anything,1 is big,2 is small
 
-			this.shotPause = _tick;
-		}
+        public void SetShotPause(decimal _tick)
+        {
+            if (_tick < 0)
+            {
+                _tick = 0m;
+            }
 
-		public decimal Tick() {
-			return this.tick + this.shotPause;
-		}
+            this.shotPause = _tick;
+        }
 
-		public bool IsShotPausing() {
-			return this.shotPause > 0;
-		}
+        public decimal Tick()
+        {
+            return this.tick + this.shotPause;
+        }
 
-		public float GetAttackrangeRate() {
-			return this.attackRangeRate;
-		}
+        public bool IsShotPausing()
+        {
+            return this.shotPause > 0;
+        }
 
-		public int GetSize(){
-			return this.size;
-		}
+        public float GetAttackrangeRate()
+        {
+            return this.attackRangeRate;
+        }
 
-		public void SetAttackRangeRate(float value) {
-			this.attackRangeRate = value;
-		}
+        public int GetSize()
+        {
+            return this.size;
+        }
 
-		public bool IsPressTemp() {
-			return this.nodeData.char_action == "char_press";
-		}
+        public void SetAttackRangeRate(float value)
+        {
+            this.attackRangeRate = value;
+        }
 
-		public uint ResultPhaser(decimal passedTick) {				//(check) check attack point position here
-			decimal result = this.Tick () - passedTick;
-			decimal perfectRange = this.nodeData.hitRange [GameMusic.PERFECT_RANGE_INDEX];
-			decimal greateRange = this.nodeData.hitRange [GameMusic.GREAT_RANGE_INDEX];
-			decimal coolRange = this.nodeData.hitRange [GameMusic.COOL_RANGE_INDEX];
-			if (result < 0) {
-				return GameMusic.NONE;
-			}
+        public bool IsPressTemp()
+        {
+            return this.nodeData.char_action == "char_press";
+        }
 
-			if (this.nodeData.hitRangeAll != 0) {
-				this.attackRangeRate = (float)(result / this.nodeData.hitRangeAll);
-			}
-			//if (result < PERFECTINTERVAL) {
-			//	return MISS;
-			//}
-			if (this.nodeData.enable_jump && GameGlobal.gGameTouchPlay.IsJump ()) {
-				return GameMusic.JUMPOVER;
-			}
-			
-			if (result < perfectRange) {
-				return GameMusic.PERFECT;
-			}
-			
-			if (result < perfectRange + greateRange) {
-				return GameMusic.GREAT;
-			}
+        public uint ResultPhaser(decimal passedTick)
+        {               //(check) check attack point position here
+            decimal result = this.Tick() - passedTick;
+            decimal perfectRange = this.nodeData.hitRange[GameMusic.PERFECT_RANGE_INDEX];
+            decimal greateRange = this.nodeData.hitRange[GameMusic.GREAT_RANGE_INDEX];
+            decimal coolRange = this.nodeData.hitRange[GameMusic.COOL_RANGE_INDEX];
+            if (result < 0)
+            {
+                return GameMusic.NONE;
+            }
 
-			if (result < perfectRange + greateRange + coolRange) {
-				return GameMusic.COOL;
-			}
-			
-			return GameMusic.NONE;
-		}
-	}
+            if (this.nodeData.hitRangeAll != 0)
+            {
+                this.attackRangeRate = (float)(result / this.nodeData.hitRangeAll);
+            }
+            //if (result < PERFECTINTERVAL) {
+            //	return MISS;
+            //}
+            if (this.nodeData.enable_jump && GameGlobal.gGameTouchPlay.IsJump())
+            {
+                return GameMusic.JUMPOVER;
+            }
 
-	/// <summary>
-	/// node data reader.
-	/// 详细说明见基类
-	/// </summary>
-	public class NodeConfigReader : BaseConfigReader {
-		private const string CONFIG_NAME = "notedata";
-		// 简化版单例
-		private NodeConfigReader(){}
-		public static NodeConfigReader Instance = new NodeConfigReader();
+            if (result < perfectRange)
+            {
+                return GameMusic.PERFECT;
+            }
 
-		private const uint lenHitRange = 3;
-		// override Init
-		public override void Init () {
-			if (this.GetDataCount () > 0) {
-				return;
-			}
-			
-			// JsonData _data = this.GetJsonConfig (CONFIG_NAME);
-			JsonData _data = ConfigPool.Instance.GetConfigByName (CONFIG_NAME);
-			for (int i = 0; i <= _data.Count; i++) {
-				string k = i.ToString ();
-				NodeConfigData sd = new NodeConfigData ();
-				if (!_data.Keys.Contains (k)) {
-					this.Add (sd);
-					continue;
-				}
+            if (result < perfectRange + greateRange)
+            {
+                return GameMusic.GREAT;
+            }
 
-				sd = (NodeConfigData)this.ConfigToObject (_data [k], sd);
+            if (result < perfectRange + greateRange + coolRange)
+            {
+                return GameMusic.COOL;
+            }
 
-				// 从这里以下开始调整属性
-				sd.hit_type = GameMusic.TOUCH_ACTION_SIGNLE_PRESS;
-				if (sd.type == GameGlobal.NODE_TYPE_NONE) {
-					sd.hit_type = GameMusic.TOUCH_ACTION_NONE;
-				}
+            return GameMusic.NONE;
+        }
+    }
 
-				sd.addCombo = false;
-				sd.missCombo = false;
-				sd.isShowPlayEffect = false;
-				sd.jump_note = (sd.char_action == NodeConfigData.JUMP_ACTION);
-				foreach (uint _t in NodeConfigData.NODE_TYPE_IS_ADD_COMBO) {
-					if (sd.type == _t) {
-						sd.addCombo = true;
-						break;
-					}
-				}
+    /// <summary>
+    /// node data reader.
+    /// 详细说明见基类
+    /// </summary>
+    public class NodeConfigReader : BaseConfigReader
+    {
+        private const string CONFIG_NAME = "notedata";
 
-				foreach (uint _t in NodeConfigData.NODE_TYPE_IS_MISS_COMBO) {
-					if (sd.type == _t) {
-						sd.missCombo = true;
-						break;
-					}
-				}
+        // 简化版单例
+        private NodeConfigReader() { }
 
-				foreach (uint _t in NodeConfigData.NODE_TYPE_IS_SHOW_PLAY_EFFECT) {
-					if (sd.type == _t) {
-						sd.isShowPlayEffect = true;
-						break;
-					}
-				}
+        public static NodeConfigReader Instance = new NodeConfigReader();
 
-				sd.hitRange = new decimal[lenHitRange];
-				sd.hitRange [GameMusic.PERFECT_RANGE_INDEX] = sd.perfect_range;
-				sd.hitRange [GameMusic.GREAT_RANGE_INDEX] = sd.great_range;
-				sd.hitRange [GameMusic.COOL_RANGE_INDEX] = sd.cool_range;
-				sd.hitRangeAll = sd.perfect_range + sd.great_range + sd.cool_range;
+        private const uint lenHitRange = 3;
 
-				this.Add (sd);
-			}
-		}
+        // override Init
+        public override void Init()
+        {
+            if (this.GetDataCount() > 0)
+            {
+                return;
+            }
 
-		public static int GetNodeIdxByNodeid(string uid) {
-			int len = ConfigPool.Instance.GetConfigByName (CONFIG_NAME).Count;
-			for (int i = 1; i <= len; i++) {
-				if (uid == ConfigPool.Instance.GetConfigStringValue (CONFIG_NAME, i.ToString (), "uid")) {
-					return i;
-				}
-			}
+            // JsonData _data = this.GetJsonConfig (CONFIG_NAME);
+            JsonData _data = ConfigPool.Instance.GetConfigByName(CONFIG_NAME);
+            for (int i = 0; i <= _data.Count; i++)
+            {
+                string k = i.ToString();
+                NodeConfigData sd = new NodeConfigData();
+                if (!_data.Keys.Contains(k))
+                {
+                    this.Add(sd);
+                    continue;
+                }
 
-			return 0;
-		}
-	}
+                sd = (NodeConfigData)this.ConfigToObject(_data[k], sd);
 
+                // 从这里以下开始调整属性
+                sd.hit_type = GameMusic.TOUCH_ACTION_SIGNLE_PRESS;
+                if (sd.type == GameGlobal.NODE_TYPE_NONE)
+                {
+                    sd.hit_type = GameMusic.TOUCH_ACTION_NONE;
+                }
 
-	/// <summary>
-	/// musci data reader.
-	/// 详细说明见基类
-	/// 这个实现可以作为比较复杂的数据处理的例子
-	/// </summary>
-	public class MusicConfigReader : BaseConfigReader {
-		// 简化版单例
-		private MusicConfigReader(){}
-		public static MusicConfigReader Instance = new MusicConfigReader();
+                sd.addCombo = false;
+                sd.missCombo = false;
+                sd.isShowPlayEffect = false;
+                sd.jump_note = (sd.char_action == NodeConfigData.JUMP_ACTION);
+                foreach (uint _t in NodeConfigData.NODE_TYPE_IS_ADD_COMBO)
+                {
+                    if (sd.type == _t)
+                    {
+                        sd.addCombo = true;
+                        break;
+                    }
+                }
 
-		private const int tickLimit = 2;
-		// override Init
-		public override void Init (ref string filename) {
-			if (this.GetDataCount () > 0) {
-				return;
-			}
+                foreach (uint _t in NodeConfigData.NODE_TYPE_IS_MISS_COMBO)
+                {
+                    if (sd.type == _t)
+                    {
+                        sd.missCombo = true;
+                        break;
+                    }
+                }
 
-			NodeConfigReader.Instance.Init ();
-			ArrayList nodeData = NodeConfigReader.Instance.GetData ();
-			JsonData _data = ConfigPool.Instance.GetConfigByName (filename);
-			// Start from 1
-			this.Add (new MusicData ());
-			for (int i = 0; i < _data.Count; i++) {
-				MusicConfigData sd = new MusicConfigData ();
-				sd = (MusicConfigData)this.ConfigToObject (_data [i], sd);
-				if (sd.Timming < tickLimit) {
-					Debug.Assert (false, "关卡配置" + filename + "存在Timming少于2秒的node点 : " + i);
-					continue;
-				}
+                foreach (uint _t in NodeConfigData.NODE_TYPE_IS_SHOW_PLAY_EFFECT)
+                {
+                    if (sd.type == _t)
+                    {
+                        sd.isShowPlayEffect = true;
+                        break;
+                    }
+                }
 
-				MusicData md = new MusicData ();
-				md.objId = sd.Beat;
-				md.tick = decimal.Round (sd.Timming, 2);
-				md.configData = sd;
-				md.SetAttackRangeRate (-1f);
+                sd.hitRange = new decimal[lenHitRange];
+                sd.hitRange[GameMusic.PERFECT_RANGE_INDEX] = sd.perfect_range;
+                sd.hitRange[GameMusic.GREAT_RANGE_INDEX] = sd.great_range;
+                sd.hitRange[GameMusic.COOL_RANGE_INDEX] = sd.cool_range;
+                sd.hitRangeAll = sd.perfect_range + sd.great_range + sd.cool_range;
 
-				string nodeId = md.configData.CubeType;
-				if (nodeId == null) {
-					Debug.Assert (false, "关卡配置" + filename + "存在无效CubeType : " + i);
-				}
+                this.Add(sd);
+            }
+        }
 
-				foreach (NodeConfigData _nd in nodeData) {
-					if (_nd.uid == nodeId) {
-						md.nodeData = _nd;
-						break;
-					}
-				}
+        public static int GetNodeIdxByNodeid(string uid)
+        {
+            int len = ConfigPool.Instance.GetConfigByName(CONFIG_NAME).Count;
+            for (int i = 1; i <= len; i++)
+            {
+                if (uid == ConfigPool.Instance.GetConfigStringValue(CONFIG_NAME, i.ToString(), "uid"))
+                {
+                    return i;
+                }
+            }
 
-				this.Add (md);
-			}
-		}
+            return 0;
+        }
+    }
 
-		public override ArrayList GetData (ref string key) {
-			this.ClearData ();
-			this.Init (ref key);
-			return this.GetData ();
-		}
+    /// <summary>
+    /// musci data reader.
+    /// 详细说明见基类
+    /// 这个实现可以作为比较复杂的数据处理的例子
+    /// </summary>
+    public class MusicConfigReader : BaseConfigReader
+    {
+        // 简化版单例
+        private MusicConfigReader() { }
 
-		public override object GetData (int idx) {
-			if (idx < 0) {
-				return null;
-			}
+        public static MusicConfigReader Instance = new MusicConfigReader();
 
-			ArrayList data = this.GetData ();
-			if (data == null || idx > data.Count) {
-				return null;
-			}
+        private const int tickLimit = 2;
 
-			return data [idx];
-		}
-	}
+        // override Init
+        public override void Init(ref string filename)
+        {
+            if (this.GetDataCount() > 0)
+            {
+                return;
+            }
+
+            NodeConfigReader.Instance.Init();
+            ArrayList nodeData = NodeConfigReader.Instance.GetData();
+            JsonData _data = ConfigPool.Instance.GetConfigByName(filename);
+            // Start from 1
+            this.Add(new MusicData());
+            for (int i = 0; i < _data.Count; i++)
+            {
+                MusicConfigData sd = new MusicConfigData();
+                sd = (MusicConfigData)this.ConfigToObject(_data[i], sd);
+                if (sd.Timming < tickLimit)
+                {
+                    Debug.Assert(false, "关卡配置" + filename + "存在Timming少于2秒的node点 : " + i);
+                    continue;
+                }
+
+                MusicData md = new MusicData();
+                md.objId = sd.Beat;
+                md.tick = decimal.Round(sd.Timming, 2);
+                md.configData = sd;
+                md.SetAttackRangeRate(-1f);
+
+                string nodeId = md.configData.CubeType;
+                if (nodeId == null)
+                {
+                    Debug.Assert(false, "关卡配置" + filename + "存在无效CubeType : " + i);
+                }
+
+                foreach (NodeConfigData _nd in nodeData)
+                {
+                    if (_nd.uid == nodeId)
+                    {
+                        md.nodeData = _nd;
+                        break;
+                    }
+                }
+
+                this.Add(md);
+            }
+        }
+
+        public override ArrayList GetData(ref string key)
+        {
+            this.ClearData();
+            this.Init(ref key);
+            return this.GetData();
+        }
+
+        public override object GetData(int idx)
+        {
+            if (idx < 0)
+            {
+                return null;
+            }
+
+            ArrayList data = this.GetData();
+            if (data == null || idx > data.Count)
+            {
+                return null;
+            }
+
+            return data[idx];
+        }
+    }
 }
