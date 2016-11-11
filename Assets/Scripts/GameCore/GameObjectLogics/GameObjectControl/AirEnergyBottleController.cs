7c1db94e384b39ce3af3cddebced14d0c88ac868
huangwenhui <815599079@qq.com>
2016-11-11 15:44:45 +0800

    血量恢复显示和空中血量恢复bug修复

--- Assets/Scripts/GameCore/GameObjectLogics/GameObjectControl/AirEnergyBottleController.cs
+++ Assets/Scripts/GameCore/GameObjectLogics/GameObjectControl/AirEnergyBottleController.cs
@@ -1,88 +1,107 @@
-﻿using System;
+﻿using FormulaBase;
 using GameLogic;
+using System;
 using System.Collections;
 using UnityEngine;
-using FormulaBase;
 
-public class AirEnergyBottleController : BaseEnemyObjectController {
+public class AirEnergyBottleController : BaseEnemyObjectController
+{
+    private bool isBeAttacked = false;
+    private static GameObject recoveryEffect = null;
 
-	private bool isBeAttacked = false;
-	private static GameObject recoveryEffect = null;
-	void Start (){
-		if (recoveryEffect == null) {
-			int recoverItemIdx = 4000;
-			string path = "Prefabs/skill/Skill_hp";
-			recoveryEffect = StageBattleComponent.Instance.AddObjWithControllerInit (ref path, recoverItemIdx);
-			recoveryEffect.SetActive(false);
-		}
-	}
+    private void Start()
+    {
+        if (recoveryEffect == null)
+        {
+            int recoverItemIdx = 4000;
+            string path = "Prefabs/skill/Skill_hp";
+            recoveryEffect = StageBattleComponent.Instance.AddObjWithControllerInit(ref path, recoverItemIdx);
+            recoveryEffect.SetActive(false);
+        }
+    }
 
-	public override void OnControllerStart(){
-		base.OnControllerStart ();
+    public override void OnControllerStart()
+    {
+        base.OnControllerStart();
 
-		//set bottle size flag here
-		FormulaHost host = BattleEnemyManager.Instance.Enemies [this.idx];
-		if (host != null) {
-			host.SetDynamicData ("BOTTLE_TYPE", 2);
-		}
-	}
+        //set bottle size flag here
+        FormulaHost host = BattleEnemyManager.Instance.Enemies[this.idx];
+        if (host != null)
+        {
+            host.SetDynamicData("BOTTLE_TYPE", 2);
+        }
+    }
 
-	private void RecoveryEffects(){
-		var myHost = BattleRoleAttributeComponent.Instance.GetBattleRole ();
-		//int recoverValue = (int)myHost.Result (FormulaKeys.FORMULA_47);
-		//BattleRoleAttributeComponent.Instance.AddHp (recoverValue);
+    private void RecoveryEffects()
+    {
+        var uid = BattleEnemyManager.Instance.GetNodeUidByIdx(this.idx);
+        var recoverValue = 0;
+        var noteConfig = ConfigPool.Instance.GetConfigByName("notedata");
+        for (int i = 1; i <= noteConfig.Count; i++)
+        {
+            var note = noteConfig[i];
+            if (note["uid"].ToString() == uid)
+            {
+                recoverValue = (int)note["value"];
+                break;
+            }
+        }
 
-		BattleEnemyManager.Instance.SetPlayResult (this.idx, GameMusic.PERFECT);
-		BattleRoleAttributeComponent.Instance.FireSkill (SkillComponent.ON_EAT_ITEM);
+        BattleRoleAttributeComponent.Instance.AddHp(recoverValue);
 
-		recoveryEffect.SetActive (true);
+        BattleEnemyManager.Instance.SetPlayResult(this.idx, GameMusic.PERFECT);
+        BattleRoleAttributeComponent.Instance.FireSkill(SkillComponent.ON_EAT_ITEM);
 
-		SpineActionController.Play (ACTION_KEYS.COMEIN, recoveryEffect);
-		SpineActionController.Play (ACTION_KEYS.COMEOUT, this.gameObject);
+        recoveryEffect.SetActive(true);
 
+        SpineActionController.Play(ACTION_KEYS.COMEIN, recoveryEffect);
+        SpineActionController.Play(ACTION_KEYS.COMEOUT, this.gameObject);
 
-		string audioName = BattleEnemyManager.Instance.GetNodeAudioByIdx (this.idx);
-		AudioManager.Instance.PlayGirlHitByName (audioName);
-	}
-	
-//	private IEnumerator WaitForEatEnergy(float waitTime){
-//		yield return new WaitForSeconds(waitTime);
-//		RecoveryEffects();
-//	}
+        string audioName = BattleEnemyManager.Instance.GetNodeAudioByIdx(this.idx);
+        AudioManager.Instance.PlayGirlHitByName(audioName);
+    }
 
-	public override void OnControllerAttacked (int result, bool isDeaded) {
-		if (!isBeAttacked) {
-			//Debug.Log (" air energy item be attacked! ");
-			this.RecoveryEffects ();
-			SpineActionController.Play (ACTION_KEYS.COMEOUT3, this.gameObject);
-			Animator ani = this.gameObject.GetComponent<Animator> ();
-			if (ani != null) {
-				ani.speed = 0;
-			}
+    //	private IEnumerator WaitForEatEnergy(float waitTime){
+    //		yield return new WaitForSeconds(waitTime);
+    //		RecoveryEffects();
+    //	}
 
-			isBeAttacked = true;
-			TaskStageTarget.Instance.AddEnergyItemCount (1);
-		}
+    public override void OnControllerAttacked(int result, bool isDeaded)
+    {
+        if (!isBeAttacked)
+        {
+            //Debug.Log (" air energy item be attacked! ");
+            this.RecoveryEffects();
+            SpineActionController.Play(ACTION_KEYS.COMEOUT3, this.gameObject);
+            Animator ani = this.gameObject.GetComponent<Animator>();
+            if (ani != null)
+            {
+                ani.speed = 0;
+            }
 
-//		ArrayList musicData = StageBattleComponent.Instance.GetMusicData ();
-//		MusicData md = (MusicData)musicData [this.idx];
-//		// if is air unit, only jump can pass;
-//		// if is not air unit, only jump not pass.
-//		if (md.nodeData.isAirunits) {
-//			if (!GameGlobal.gGameTouchPlay.IsJump ()) {
-//				return;
-//			}
-//
-//			BattleEnemyManager.Instance.SetPlayResult(this.idx, (uint)result);
-//			var time = GameGlobal.JUMP_WHOLE_TIME * md.GetAttackrangeRate();
-//			StartCoroutine(WaitForEatEnergy(time));
-//		}
+            isBeAttacked = true;
+            TaskStageTarget.Instance.AddEnergyItemCount(1);
+        }
 
-//		string audioName = StageBattleComponent.Instance.GetNodeAudioByIdx (this.idx);
-//		AudioManager.Instance.PlayGirlHitByName (audioName);
-		
-//		GameGlobal.gGameMusicScene.ShowLongPressMask (false);
+        //		ArrayList musicData = StageBattleComponent.Instance.GetMusicData ();
+        //		MusicData md = (MusicData)musicData [this.idx];
+        //		// if is air unit, only jump can pass;
+        //		// if is not air unit, only jump not pass.
+        //		if (md.nodeData.isAirunits) {
+        //			if (!GameGlobal.gGameTouchPlay.IsJump ()) {
+        //				return;
+        //			}
+        //
+        //			BattleEnemyManager.Instance.SetPlayResult(this.idx, (uint)result);
+        //			var time = GameGlobal.JUMP_WHOLE_TIME * md.GetAttackrangeRate();
+        //			StartCoroutine(WaitForEatEnergy(time));
+        //		}
 
-		return;
-	}
+        //		string audioName = StageBattleComponent.Instance.GetNodeAudioByIdx (this.idx);
+        //		AudioManager.Instance.PlayGirlHitByName (audioName);
+
+        //		GameGlobal.gGameMusicScene.ShowLongPressMask (false);
+
+        return;
+    }
 }
